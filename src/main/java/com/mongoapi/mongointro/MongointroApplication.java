3474bea478b9382ff1787513bf76d58a1cf87865
martin-ngigi <martinwainaina001@gmail.com>
2022-12-27 21:11:10 +0300

    Initial commit

---
+++ src/main/java/com/mongoapi/mongointro/MongointroApplication.java
@@ -0,0 +1,13 @@
+package com.mongoapi.mongointro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongointroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongointroApplication.class, args);
+	}
+
+}
